ee527d0c910d1d27311ab45257332d5b8b65419d
Xavier Noria <fxn@hashref.com>
2021-05-30 22:07:01 +0200

    Extract loader helpers to a separate module
    Next step in trimming lib/zeitwerk/loader.rb is to move auxiliary methods out.
    
    With this, we reach a sweet spot in which the core loader implementation is what
    remains in the file. This is interesting, because the bulk of the public API is
    configuration, but that's in a mixin. From the point of view of the library, the
    main code is the one that does the actual work, everything else is auxiliary.
    That's the code I want to see when I open lib/zeitwerk/loader.rb.

--- lib/zeitwerk/loader.rb
+++ lib/zeitwerk/loader.rb
@@ -5,11 +5,13 @@ require "securerandom"
 
 module Zeitwerk
   class Loader
+    require_relative "loader/helpers"
     require_relative "loader/callbacks"
     require_relative "loader/config"
 
     include RealModName
     include Callbacks
+    include Helpers
     include Config
 
     # @private
@@ -406,85 +408,6 @@ module Zeitwerk
       end
     end
 
-    # The autoload? predicate takes into account the ancestor chain of the
-    # receiver, like const_defined? and other methods in the constants API do.
-    #
-    # For example, given
-    #
-    #   class A
-    #     autoload :X, "x.rb"
-    #   end
-    #
-    #   class B < A
-    #   end
-    #
-    # B.autoload?(:X) returns "x.rb".
-    #
-    # We need a way to strictly check in parent ignoring ancestors.
-    #
-    # @sig (Module, Symbol) -> String?
-    if method(:autoload?).arity == 1
-      def strict_autoload_path(parent, cname)
-        parent.autoload?(cname) if cdef?(parent, cname)
-      end
-    else
-      def strict_autoload_path(parent, cname)
-        parent.autoload?(cname, false)
-      end
-    end
-
-    # @sig (Module, Symbol) -> String
-    if Symbol.method_defined?(:name)
-      # Symbol#name was introduced in Ruby 3.0. It returns always the same
-      # frozen object, so we may save a few string allocations.
-      def cpath(parent, cname)
-        Object == parent ? cname.name : "#{real_mod_name(parent)}::#{cname.name}"
-      end
-    else
-      def cpath(parent, cname)
-        Object == parent ? cname.to_s : "#{real_mod_name(parent)}::#{cname}"
-      end
-    end
-
-    # @sig (String) { (String, String) -> void } -> void
-    def ls(dir)
-      Dir.each_child(dir) do |basename|
-        next if hidden?(basename)
-
-        abspath = File.join(dir, basename)
-        next if ignored_paths.member?(abspath)
-
-        # We freeze abspath because that saves allocations when passed later to
-        # File methods. See #125.
-        yield basename, abspath.freeze
-      end
-    end
-
-    def hidden?(basename)
-      basename.start_with?(".")
-    end
-
-    # @sig (String) -> bool
-    def ruby?(path)
-      path.end_with?(".rb")
-    end
-
-    # @sig (String) -> bool
-    def dir?(path)
-      File.directory?(path)
-    end
-
-    # @sig (String) -> void
-    def log(message)
-      method_name = logger.respond_to?(:debug) ? :debug : :call
-      logger.send(method_name, "Zeitwerk@#{tag}: #{message}")
-    end
-
-    # @sig (Module, Symbol) -> bool
-    def cdef?(parent, cname)
-      parent.const_defined?(cname, false)
-    end
-
     # @sig (String) -> void
     def register_explicit_namespace(cpath)
       ExplicitNamespace.register(cpath, self)
@@ -503,18 +426,6 @@ module Zeitwerk
           end
         end
       end
-    end
-
-    # @sig (Module, Symbol) -> void
-    def unload_autoload(parent, cname)
-      parent.__send__(:remove_const, cname)
-      log("autoload for #{cpath(parent, cname)} removed") if logger
-    end
-
-    # @sig (Module, Symbol) -> void
-    def unload_cref(parent, cname)
-      parent.__send__(:remove_const, cname)
-      log("#{cpath(parent, cname)} unloaded") if logger
     end
   end
 end
