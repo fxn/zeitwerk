ac24066eb3480b2cfecf43f4e8ea24ee88fb711b
Xavier Noria <fxn@hashref.com>
2022-02-15 19:12:24 +0100

    Let NoWarningsPolicy work with old signatures

--- test/support/no_warnings_policy.rb
+++ test/support/no_warnings_policy.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 module NoWarningsPolicy
-  def warn(*, **)
-    super
-    abort("This test suite aborts on warnings, please fix the one above.")
+  MESSAGE = "This test suite aborts on warnings, please fix the one above."
+
+  if Warning.method(:warn).arity == 1
+    def warn(*)
+      super
+      abort(MESSAGE)
+    end
+  else
+    def warn(*, **)
+      super
+      abort(MESSAGE)
+    end
   end
 end
 
