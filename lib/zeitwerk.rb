a3252dadc8f353ac398365ee064ab4250c8efb5c
Juan Vasquez <juan@ombulabs.com>
2024-07-02 18:27:59 -0600

    Use require instead of require_relative
    I wonder if it's "better" or more recommended using require over
    require_relative, I think it is, so, suggesting the change, happy to
    hear what is best.

--- lib/zeitwerk.rb
+++ lib/zeitwerk.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 module Zeitwerk
-  require_relative "zeitwerk/real_mod_name"
-  require_relative "zeitwerk/internal"
-  require_relative "zeitwerk/cref"
-  require_relative "zeitwerk/loader"
-  require_relative "zeitwerk/gem_loader"
-  require_relative "zeitwerk/registry"
-  require_relative "zeitwerk/explicit_namespace"
-  require_relative "zeitwerk/inflector"
-  require_relative "zeitwerk/gem_inflector"
-  require_relative "zeitwerk/null_inflector"
-  require_relative "zeitwerk/kernel"
-  require_relative "zeitwerk/error"
-  require_relative "zeitwerk/version"
+  require "zeitwerk/real_mod_name"
+  require "zeitwerk/internal"
+  require "zeitwerk/cref"
+  require "zeitwerk/loader"
+  require "zeitwerk/gem_loader"
+  require "zeitwerk/registry"
+  require "zeitwerk/explicit_namespace"
+  require "zeitwerk/inflector"
+  require "zeitwerk/gem_inflector"
+  require "zeitwerk/null_inflector"
+  require "zeitwerk/kernel"
+  require "zeitwerk/error"
+  require "zeitwerk/version"
 
   # This is a dangerous method.
   #
