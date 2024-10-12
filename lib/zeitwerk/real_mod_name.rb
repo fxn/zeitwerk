e1786b3eee66cbd2d05a1556f7be42cd62100be2
Benoit Daloze <eregontp@gmail.com>
2024-10-12 12:26:14 +0200

    Add workaround for truffleruby 24.1.0 issue
    * See https://github.com/oracle/truffleruby/issues/3683
    * Without this the test suite has 6 failures, 26 errors.

--- lib/zeitwerk/real_mod_name.rb
+++ lib/zeitwerk/real_mod_name.rb
@@ -10,7 +10,18 @@ module Zeitwerk::RealModName
   # The name method can be overridden, hence the indirection in this method.
   #
   # @sig (Module) -> String?
-  def real_mod_name(mod)
-    UNBOUND_METHOD_MODULE_NAME.bind_call(mod)
+  if RUBY_ENGINE == 'truffleruby' && (RUBY_ENGINE_VERSION.split('.').map(&:to_i) <=> [24, 2, 0]) < 0
+    def real_mod_name(mod)
+      name = UNBOUND_METHOD_MODULE_NAME.bind_call(mod)
+      # https://github.com/oracle/truffleruby/issues/3683
+      if name && name.start_with?('Object::')
+        name = name[8..-1]
+      end
+      name
+    end
+  else
+    def real_mod_name(mod)
+      UNBOUND_METHOD_MODULE_NAME.bind_call(mod)
+    end
   end
 end
