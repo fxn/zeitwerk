d97fc46a8d0d083670ec8cd40d6660253be527f3
Genar Trias Ortiz <gtrias@gmail.com>
2024-12-13 10:18:24 +0100

    Handling top level module namespaces

--- lib/zeitwerk/loader.rb
+++ lib/zeitwerk/loader.rb
@@ -540,7 +540,7 @@ module Zeitwerk
       # FIXME: This has a performance penalization for few edge cases that need it.
       # In our project they are just 3. It would be great to be able to configure this 
       # per project or just disable it, so everybody can have the most performant experience.
-      unless ['ActionCable', 'ActionCable::Engine', 'FQL'].include?(cref.path)
+      if cref.path.start_with('ActionCable', 'FQL')
         Registry.register_inception(cref.path, abspath, self)
       end
     end
